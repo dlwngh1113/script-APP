b4f03bf05a5f432759f69603cbc45745aa235453
dlwngh1113 <dlwngh1113@naver.com>
2020-05-23 22:13:54 +0900

    grid access

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter.font import *
+from tkinter.ttk import *
 from search import *
 
 frameHeight = 580
@@ -14,22 +15,19 @@ class MainGUI:
         self.window.geometry(str(WIDTH) + 'x' + str(HEIGHT))
         self.window.resizable(False, False)
 
-        APP = Font(family='italic', size=40, slant='italic')
-        nameTag = Label(self.window, text='APP', font=APP)
-        nameTag.grid(row=0, column=0)
+        APP = Font(family='bauhaus 93', size=40, slant='italic')
+        nameTag = Label(self.window, text='APP', font=APP, width=4)
+        nameTag.grid(row=0, column=0, sticky='w')
 
-        self.searchFrame = Frame(self.window, width=WIDTH / 2 - 30, height=frameHeight, relief=GROOVE, bg='lightgray')
-        #self.searchFrame.place(x=0, y=HEIGHT - frameHeight)
+        self.searchFrame = Frame(self.window, width=WIDTH / 2 - 30, height=frameHeight, bg='lightgray')
         self.searchFrame.grid(row=1, column=0)
         SearchFrame(self.searchFrame)
 
-        self.mapFrame = Frame(self.window, width=WIDTH / 2 - 60, height=frameHeight, bg='gray')
+        self.mapFrame = Frame(self.window, width=WIDTH / 2 - 60, height=frameHeight, bg='magenta')
         self.mapFrame.grid(row=1, column=1)
-        #self.mapFrame.place(x=WIDTH / 2 - 30, y=HEIGHT - frameHeight)
 
         self.funcFrame = Frame(self.window, width=90, height=frameHeight, bg='cyan')
         self.funcFrame.grid(row=1, column=2)
-        #self.funcFrame.place(x=WIDTH - 90, y=HEIGHT - frameHeight)
 
         self.window.mainloop()
         pass
--- search.py
+++ search.py
@@ -1,13 +1,17 @@
 from xml import *
 from tkinter import *
 from tkinter.font import *
-from tkinter.ttk import *
+import tkinter.ttk
 
 
 class SearchFrame:
     def __init__(self, frame):
         self.font = Font(family='italic', slant='italic')
-        cityLabel = Label(frame, text='시/군', font=self.font, bg='gray')
+        cityLabel = Label(frame, text='시/군', font=self.font)
         cityLabel.place(x=30, y=30)
-        cityCombo = Combobox()
+        values = [str(i) for i in range(10)]
+        cityCombo = tkinter.ttk.Combobox(frame, values=values, height=5)
+        cityCombo.configure(state='readonly')
+        cityCombo.place(x=100, y=30)
+        cityCombo.set('목록 선택')
         pass
