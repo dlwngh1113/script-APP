ab87b73932d9b6db6d02794751921bb1d9fd5120
jaewon-lee-ssb <jaewon990905@gmail.com>
2020-05-28 17:55:50 +0900

    JobLabel init

--- search.py
+++ search.py
@@ -6,12 +6,24 @@ import tkinter.ttk
 
 class SearchFrame:
     def __init__(self, frame):
-        self.font = Font(family='italic', slant='italic')
+        self.font = Font(family='italic', weight='bold', slant='roman')
         cityLabel = Label(frame, text='시/군', font=self.font)
-        cityLabel.place(x=30, y=30)
+        cityLabel.place(x=30, y=25)
         values = [str(i) for i in range(10)]
         cityCombo = tkinter.ttk.Combobox(frame, values=values, height=5)
         cityCombo.configure(state='readonly')
         cityCombo.place(x=100, y=30)
         cityCombo.set('목록 선택')
+
+        jobLabel = Label(frame, text='업종', font=self.font)
+        jobLabel.place(x=30, y=80)
+        values = [str(i) for i in range(10)]
+        cityCombo = tkinter.ttk.Combobox(frame, values=values, height=5)
+        cityCombo.configure(state='readonly')
+        cityCombo.place(x=100, y=85)
+        cityCombo.set('목록 선택')
+
+    def search(self):
         pass
+
+
