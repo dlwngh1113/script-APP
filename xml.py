3164ef880872127fffbcae5b7e1f6495f87dbe58
jaewon-lee-ssb <jaewon990905@gmail.com>
2020-06-07 18:07:18 +0900

    xml 파일 파싱

---
+++ xml.py
@@ -0,0 +1,11 @@
+import xml.etree.ElementTree as ET
+tree = ET.parse('academy.xml')
+root = tree.getroot()
+
+row = root.findall("row")
+
+cityName = [x.findtext("SIGUN_NM") for x in row]
+cityDong = [x.findtext("EMD_NM") for x in row]
+
+print(cityName)
+print(cityDong)
