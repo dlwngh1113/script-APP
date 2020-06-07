48daffef156cbfdb793fb9c7ff11aab81a40217a
dlwngh1113 <dlwngh1113@naver.com>
2020-06-07 19:39:38 +0900

    parsing success

--- xml.py
+++ xml.py
@@ -11,4 +11,18 @@ print(cityName)
 print(cityDong)'''
 
 import requests
-url = 'https://openapi.gg.go.kr/TninsttInstutM'
+from bs4 import BeautifulSoup
+import urllib
+
+open_api_key = '28c84783ab7249d9b2251c0bb4bc525b'
+SIGUN_NM = urllib.parse.quote(input())
+params = '&SIGUN_NM='+SIGUN_NM
+url = 'https://openapi.gg.go.kr/TninsttInstutM?KEY='
+open_url = url + open_api_key + params
+
+req = requests.get(open_url)
+html = req.text
+
+soup = BeautifulSoup(html, 'html.parser')
+values = soup.find_all('faclt_nm')
+print([x.text for x in values][:5])
