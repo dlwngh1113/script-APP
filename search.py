5d1259938b33ae1b1e5829bb77b20cdc65fd546d
dlwngh1113 <dlwngh1113@naver.com>
2020-06-07 21:02:16 +0900

    listbox complete

--- search.py
+++ search.py
@@ -2,7 +2,12 @@ from xml import *
 from tkinter import *
 from tkinter.font import *
 import tkinter.ttk
+import requests
+from bs4 import BeautifulSoup
+import urllib
 
+open_api_key = '28c84783ab7249d9b2251c0bb4bc525b'
+url = 'https://openapi.gg.go.kr/TninsttInstutM?KEY='
 
 class SearchFrame:
     def __init__(self, frame):
@@ -10,26 +15,24 @@ class SearchFrame:
         self.Labelfont = Font(family='italic', weight='bold', slant='roman', size=20)
 
         # 시/군 라벨 생성, 위치 지정
-        cityLabel = Label(frame, text='시/군', font=self.Labelfont)
-        cityLabel.place(x=15, y=25)
+        Label(frame, text='시/군', font=self.Labelfont).place(x=15, y=25)
         #행정구역 더 추가해야함
         cities = ['고양시', '과천시', '광명시']
-        cityCombo = tkinter.ttk.Combobox(frame, values=cities, height=5, font=self.Combofont)
-        cityCombo.configure(state='readonly')
-        cityCombo.place(x=90, y=30)
+        self.cityCombo = tkinter.ttk.Combobox(frame, values=cities, height=5, font=self.Combofont)
+        self.cityCombo.configure(state='readonly')
+        self.cityCombo.place(x=90, y=30)
         frame.option_add('*TCombobox*Listbox.font', self.Combofont)
-        cityCombo.set('목록 선택')
+        self.cityCombo.set('목록 선택')
 
         # 업종 라벨 생성, 위치 지정
-        jobLabel = Label(frame, text='업종', font=self.Labelfont)
-        jobLabel.place(x=15, y=80)
+        Label(frame, text='업종', font=self.Labelfont).place(x=15, y=80)
         #업종
         industries = ['교습소', '평생직업교육학원', '학교교과교습학원']
-        cityCombo = tkinter.ttk.Combobox(frame, values=industries, height=3, font=self.Combofont)
-        cityCombo.configure(state='readonly')
-        cityCombo.place(x=90, y=85)
+        self.industryCombo = tkinter.ttk.Combobox(frame, values=industries, height=3, font=self.Combofont)
+        self.industryCombo.configure(state='readonly')
+        self.industryCombo.place(x=90, y=85)
         frame.option_add('*TCombobox*Listbox.font', self.Combofont)
-        cityCombo.set('목록 선택')
+        self.industryCombo.set('목록 선택')
 
         # 검색 버튼 생성, 위치 지정
         Button(frame, text='검색', width=4, height=1, relief='raised', bd=4, font=self.Labelfont,
@@ -43,4 +46,18 @@ class SearchFrame:
         self.academyListBox.place(x=15, y=230)
 
     def search(self):
-        pass
+        self.academyListBox.delete(0, self.academyListBox.size())
+        SIGUN_NM = urllib.parse.quote(self.cityCombo.get())
+        INDUTYPE = urllib.parse.quote(self.industryCombo.get())
+        params = '&SIGUN_NM=' + SIGUN_NM + '&INDUTYPE_DIV_NM=' + INDUTYPE
+        open_url = url + open_api_key + params
+
+        req = requests.get(open_url)
+        html = req.text
+
+        soup = BeautifulSoup(html, 'html.parser')
+        values = soup.find_all('faclt_nm')
+
+        valueList = [x.text for x in values]
+        for x in range(len(valueList)):
+            self.academyListBox.insert(x, valueList[x])
